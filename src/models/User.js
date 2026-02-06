efef45f8ed114489dbba078645809f0ef7a20804
Deepak Yadav <deepak226713@gmail.com>
2026-02-06 13:09:44 +0530

    work on profilePicture - default null

--- src/models/User.js
+++ src/models/User.js
@@ -73,7 +73,7 @@ const userSchema = new mongoose.Schema(
       resumeUrl: { type: String, trim: true },
       profilePicture: { 
         type: String, 
-        default: 'https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png' 
+        default: null,
       },
       website: { type: String, trim: true },
       location: { type: String, trim: true },
